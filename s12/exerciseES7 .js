b5f5063303883bd0cdc2ab706a3d3721e08a0a3d
tomaka <htomaka@treeptik.fr>
2018-03-22 21:33:23 +0100

    remove duplicate array

--- s12/exerciseES7 .js
+++ s12/exerciseES7 .js
@@ -2,14 +2,13 @@ const assert = require ('assert');
 
 // Solve the below problems:
 // #1) Check if this array includes the name "John".
-const dragons1 = ['Tim', 'Johnathan', 'Sandy', 'Sarah'];
-const hasJohn = dragons1.some(dragon => dragon.includes('John'));
+const dragons = ['Tim', 'Johnathan', 'Sandy', 'Sarah'];
+const hasJohn = dragons.some(dragon => dragon.includes('John'));
 assert(hasJohn);
 
 // #2) Check if this array includes any name that has "John" inside of it. If it does, return that
 // name or names in an array.
-const dragons2 = ['Tim', 'Johnathan', 'Sandy', 'Sarah'];
-const match = dragons1.filter(dragon => dragon.includes('John'));
+const match = dragons.filter(dragon => dragon.includes('John'));
 assert.deepEqual(match, ['Johnathan']);
 
 // #3) Create a function that calulates the power of 100 of a number entered as a parameter
